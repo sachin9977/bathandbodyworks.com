835ceb3545cca7b30328bd65127f25bc02022430
sachin9977 <sc09626@gmail.com>
2022-09-30 20:36:07 +0530

    mistake

--- bathandbodyworks/src/Components/HomePage.jsx
+++ bathandbodyworks/src/Components/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
-import SignupLogin from "./SignupLogin"
 
 function HomePage() {
   return (
     <div>
-        <SignupLogin/>
+        <Navbar/>
     </div>
   )
 }
